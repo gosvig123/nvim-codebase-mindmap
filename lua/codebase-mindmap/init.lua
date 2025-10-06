ce72f122d9f14284bce84aa1908972b61d8f9093
kristian gosvig <leasemobil@gmail.com>
2025-10-06 16:20:23 +0200

    Add CodebaseMindmapFunction and CodebaseMindmapOverview commands
    - Add :CodebaseMindmapFunction command for function call graph view
    - Add :CodebaseMindmapOverview command for file overview mode
    - Keep backward compatibility with :MindMap command

--- lua/codebase-mindmap/init.lua
+++ lua/codebase-mindmap/init.lua
@@ -11,6 +11,14 @@ M.config = {
 function M.setup(opts)
   M.config = vim.tbl_deep_extend("force", M.config, opts or {})
   
+  vim.api.nvim_create_user_command("CodebaseMindmapFunction", function()
+    require("codebase-mindmap.ui").show_function_graph()
+  end, {})
+
+  vim.api.nvim_create_user_command("CodebaseMindmapOverview", function()
+    require("codebase-mindmap.ui").show_overview()
+  end, {})
+
   vim.api.nvim_create_user_command("MindMap", function(args)
     if args.args == "file" then
       require("codebase-mindmap.ui").show_file_map()
